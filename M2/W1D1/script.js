79b5d90ce2ac0d642f0ec6f3178e19453beb0957
Simone Greco <greco.simone@live.it>
2024-07-14 16:56:16 +0200

    nuova revisione

---
+++ M2/W1D1/script.js
@@ -0,0 +1,9 @@
+/*
+1)
+Un algoritmo è una sequenza di istruzioni, per determinare un risultato.
+2)
+Una variabile la possiamo immaginare come una scatola al cui interno vengono conservati dei valori.
+3)
+No, non sono la stessa cosa, perché UNDEFINED come dice la parola indica un valore non definito e che quindi può essere tutto o niente.
+Invece NULL indica il nulla o il vuoto.
+*/
---
+++ M2/W1D4/s1p2-scaricabile.js
@@ -0,0 +1,80 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*
+I datatypes possono essere di vari tipi:
+-String, se devono contenere una parola;
+-Number, per contenere numeri di qualsiasi tipo (es. interi o con la virgola);
+-Boolean, se deve comprendere uno stato TRUE o FALSE.
+-NULL, può essere assegnato a una variabile per specificare che il suo valore è nullo.
+-UNDEFINED, può essere la risposta di una variabile se il suo valore non è stato specificato.
+*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+/*Un oggetto in JS è come se fosse una scatola al cui interno è possibile mettere altre scatole. In modo tale da poter far contenere a un oggetto più variabili di diversa specie.*/
+
+/* ESERCIZIO 3
+ Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+console.log(12+20)
+
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
+
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name = "Simone"
+
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+console.log(4-x)
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ Infine, verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let name1 = "john"
+let name2 = "John"
+
+console.log(name1==name2)
+
+name1 = name1.toLowerCase()
+name2 = name2.toLowerCase()
+
+console.log(name1==name2)
